1dd11b88b78a127b8b8366f3dd86d2969c9eea45
Rohit Mahindrakar <rohitcyablord@users.noreply.github.com>
2021-12-06 20:05:35 +0530

    updates 1

--- src/app/work-details/work-details.component.ts
+++ src/app/work-details/work-details.component.ts
@@ -9,7 +9,93 @@ export class WorkDetailsComponent implements OnInit {
 
   constructor() { }
 
+  public voice: boolean = false;
+  public write: boolean = true;
+  public act: boolean = true;
+  public host: boolean = true;
+  public lecture: boolean = true;
+  public model: boolean = true;
+  public direct: boolean = true;
+
   ngOnInit(): void {
+  }
+
+  subNavigation(toWhere: string) {
+
+    switch (toWhere) {
+      case "voice":
+        this.voice = false;
+        this.write = true;
+        this.act = true;
+        this.host = true;
+        this.lecture = true;
+        this.model = true;
+        this.direct = true;
+        break;
+      case "write":
+        this.voice = true;
+        this.write = false;
+        this.act = true;
+        this.host = true;
+        this.lecture = true;
+        this.model = true;
+        this.direct = true;
+        break;
+      case "act":
+        this.voice = true;
+        this.write = true;
+        this.act = false;
+        this.host = true;
+        this.lecture = true;
+        this.model = true;
+        this.direct = true;
+        break;
+      case "host":
+        this.voice = true;
+        this.write = true;
+        this.act = true;
+        this.host = false;
+        this.lecture = true;
+        this.model = true;
+        this.direct = true;
+        break;
+      case "lecture":
+        this.voice = true;
+        this.write = true;
+        this.act = true;
+        this.host = true;
+        this.lecture = false;
+        this.model = true;
+        this.direct = true;
+        break;
+      case "model":
+        this.voice = true;
+        this.write = true;
+        this.act = true;
+        this.host = true;
+        this.lecture = true;
+        this.model = false;
+        this.direct = true;
+        break;
+      case "direct":
+        this.voice = true;
+        this.write = true;
+        this.act = true;
+        this.host = true;
+        this.lecture = true;
+        this.model = true;
+        this.direct = false;
+        break;
+      default:
+        this.voice = false;
+        this.write = true;
+        this.act = true;
+        this.host = true;
+        this.lecture = true;
+        this.model = true;
+        this.direct = true;
+    }
+
   }
 
 }
